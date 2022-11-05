7329403e3109707e6e6c28ca032ff39716526b52
Tornado <71680818+wanan-an@users.noreply.github.com>
2022-11-05 20:32:09 +0800

    Add files via upload

--- chufeng.cpp
+++ chufeng.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <fstream>
+#include <sstream>
+#include <stack>
 #include <windows.h>
 using namespace std;
 
@@ -14,16 +17,18 @@ struct Node {
 };
 
 struct  Qtree {
-    int depth;
-    int width;
     Node* root;
     Qtree(Node* _root) :root(_root) {}
-    Qtree() {}
+    Qtree() = default;
 };
 
-vector<Qtree> TreeList;
-vector<Qtree> TreeList2;
-vector<vector<int>> adjList;
+vector<vector<Qtree>> TreeLst1;
+vector<vector<Qtree>> TreeLst2;
+vector<vector<int>> adjList(2050);
+int nodenum;
+stack<int> node_stack;
+int visit[2050] = { 0 };
+vector<vector<int>> paths;
 
 
 //判断两个数组是否有相同元素，若有则输出一个即可
@@ -50,6 +55,7 @@ bool search(vector<int> lst, int ans) {
     return (find(lst.begin(), lst.end(), ans) != lst.end());
 }
 
+
 //判断某个结点的子树中是否有和qset不冲突的结点，若有则选择一个
 vector<int> Find(Node* node, vector<int> qset) {
     int ans = compare(node->edges, qset);
@@ -66,51 +72,80 @@ vector<int> Find(Node* node, vector<int> qset) {
     }
 }
 
-
-void preparetion() {
-    adjList.push_back({});
-    adjList.push_back({ 2,5,6,7,9 });
-    adjList.push_back({ 1,4,9 });
-    adjList.push_back({ 6,8,9 });
-    adjList.push_back({ 2,7,8,9,10 });
-    adjList.push_back({ 1,6,10 });
-    adjList.push_back({ 1,3,5,10 });
-    adjList.push_back({ 1,4,10 });
-    adjList.push_back({ 3,4,10 });
-    adjList.push_back({ 1,2,3,4 });
-    adjList.push_back({ 4,5,6,7,8 });
-    for (int i = 0; i < 10; i++) {
-        if (find(adjList[10].begin(), adjList[10].end(), i) != adjList[10].end()) {
-            Node* node = new Node(vector<int>{});
-            TreeList.push_back(Qtree(node));
-        }
-        else {
-            TreeList.push_back(Qtree(nullptr));
+//由邻边信息生成第一批树（p=0）
+void base() {
+    for (int i = 0; i <= nodenum; i++) {
+        TreeLst1.push_back({});
+        TreeLst2.push_back({});
+        for (int j = 0; j <= nodenum; j++) {
+            if (i!=j && (find(adjList[i].begin(), adjList[i].end(), j) != adjList[i].end())) {
+                Node* node = new Node(vector<int>{});
+                TreeLst1[i].push_back(Qtree(node));
+            }
+            else {
+                TreeLst1[i].push_back(Qtree(nullptr));
+            }
         }
     }
+    cout << "prepare successfully" << endl;
 }
 
+void prepare(string filepath) {
+    for (int i = 0; i < 2050; i++) {
+        adjList[i] = {};
+    }
+    string line;
+    ifstream _input(filepath, ios::in);
+    if (!_input) {
+        cout << "File not exist!" << endl;
+        return;
+    }
+    getline(_input, line);
+    while (getline(_input, line)) {
+        istringstream sin(line);
+        string t;
+        int startnode, endnode;
+        int index = 0;
+        while (sin >> t) {
+            if (index == 0) {
+                startnode = stoi(t);
+                index++;
+            }
+            else {
+                endnode = stoi(t);
+                break;
+            }
+        }
+        adjList[startnode].push_back(endnode);
+        adjList[endnode].push_back(startnode);
+    }
+    cout << "input edge information successfully!" << endl;
+}
 
-
-
+//由上一级的树生成下一级的树
 //start表示初始结点，p表示路径中包含的中间结点数，q为代表集大小，trees表示上一级的树集
-Qtree grow(int start, int end, int p, int q, vector<Qtree> trees) {
+Qtree grow(int start, int end, int p, int q, vector<vector<Qtree>> trees) {
     Qtree result;
     int flag = false;
     queue<Node*> QtNode;
     //生成树的根节点
-    for (int m : adjList[start]) {
-        if (trees[m].root != nullptr) {
-            flag = true;
-            vector<int> temp(trees[m].root->edges);
-            temp.insert(temp.begin(), m);
-            Node* root = new Node(temp);
-            result.root = root;
-            break;
-        }
-    }
-    if (!flag) {
+    if (start == end) {
         result.root = nullptr;
+    }
+    else {
+        for (int m : adjList[start]) {
+            if (m != end && trees[end][m].root != nullptr && !search(trees[end][m].root->edges, m) && !search(trees[end][m].root->edges,start)) {
+                flag = true;
+                vector<int> temp(trees[end][m].root->edges);
+                temp.insert(temp.begin(), m);
+                Node* root = new Node(temp);
+                result.root = root;
+                break;
+            }
+        }
+        if (!flag) {
+            result.root = nullptr;
+        }
     }
     //树为空，则不用继续往下生长
     if (result.root != nullptr) {
@@ -132,14 +167,17 @@ Qtree grow(int start, int end, int p, int q, vector<Qtree> trees) {
                     child->path.push_back(node->edges[j]);
                     for (int k : adjList[start]) {
                         /*不选取当前邻接结点的三种情况
-                        1、邻接结点为最终结点   
-                        2、从邻接结点到最终结点的路径上经过初始结点    
+                        1、邻接结点为最终结点
+                        2、从邻接结点到最终结点的路径上经过初始结点
                         3、邻接结点与最终结点间不存在经过长为p的路径*/
-                        if (k == end || search(child->path, k) || trees[k].root == nullptr) {
+                        if (k == end || search(child->path, k) || trees[end][k].root == nullptr) {
                             continue;
                         }
                         else {
-                            vector<int> ans = Find(trees[k].root, child->path);
+                            vector<int> ans = Find(trees[end][k].root, child->path);
+                            if (search(ans, k)) {
+                                continue;
+                            }
                             //此处额外在p等于1的情况下认为存在，因为p等于0的树中空集也表示一条路径
                             if ((!ans.empty() || p == 1) && !search(ans, start)) {
                                 ans.insert(ans.begin(), k);
@@ -197,22 +235,87 @@ void PrintTree(Qtree tree) {
     }
 }
 
+//查询从start到end的树，需要调换顺序
+void query(int start, int end) {
+    PrintTree(TreeLst1[end][start]);
+}
 
-int main() {
-    //TreeList.reserve(1000);
-    preparetion();
-    for (int p = 1; p < 3; p++) {
-        for (int i = 0; i < 10; i++) {
-            Qtree tree = grow(i, 10, p, 5 - p, TreeList);
-            TreeList2.push_back(tree);
+//生成所有的代表树
+void generate_all_trees(int p, int q) {
+    for (int th = 1; th <= p; th++) {
+        for (int i = 0; i <= nodenum; i++) {
+            for (int j = 0; j <= nodenum; j++) {
+                Qtree tree = grow(j, i, th, p+q-th, TreeLst1);
+                TreeLst2[i].push_back(tree);
+            }
+            TreeLst1[i].swap(TreeLst2[i]);
+            vector<Qtree>().swap(TreeLst2[i]);
         }
-        TreeList.swap(TreeList2);
-        vector<Qtree>().swap(TreeList2);
     }
     cout << "trees generated!" << endl;
-    for (int i = 0; i < TreeList.size(); i++){
-        cout << "tree" << i << ":" << endl;
-        PrintTree(TreeList[i]);
+}
+
+void printPath(vector<int> path) {
+    for (auto i : path) {
+        cout << i << " ";
     }
+    cout << endl;
+}
+
+void addPath(stack<int> s) {
+    vector<int> path;
+    stack<int> s1(s);
+    while (!s1.empty()) {
+        int node = s1.top();
+        s1.pop();
+        path.insert(path.begin(), node);
+    }
+    paths.push_back(path);
+}
+
+void FindAllPath(int startnode, int endnode, int length) {
+    if (node_stack.size() == length+1) {
+        return;
+    }
+    node_stack.push(startnode);
+    visit[startnode] = 1;
+    if (startnode == endnode) {
+        if (node_stack.size() == length+1) {
+            addPath(node_stack);
+        }
+        node_stack.pop();
+        visit[startnode] = 0;
+    }
+    else {
+        for (auto i : adjList[startnode]) {
+            if (!visit[i]) {
+                FindAllPath(i, endnode, length);
+            }
+        }
+        node_stack.pop();
+        visit[startnode] = 0;
+    } 
+}
+
+int main() {
+    int p,q,start,end;
+    cout << "please input the nodenum:";
+    cin >> nodenum;
+    string filepath = "..\\edgelist\\PA_100_edge.csv";
+    prepare(filepath);
+    base();
+    cout << "please input p and q:";
+    cin >> p >> q;
+    generate_all_trees(p, q);
+    cout << "please input the startnode and endnode:";
+    cin >> start >> end;
+    cout << "from" << start << "to" << end << " " << endl;
+    FindAllPath(start, end, p+1);
+    for (int i = 0; i < paths.size(); i++) {
+        cout << "path " << i+1 << ":";
+        printPath(paths[i]);
+    }
+    query(start, end);
+    
     return 0;
 }
