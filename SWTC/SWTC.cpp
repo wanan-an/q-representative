ca1b90e49fbb7fc3639b6882055cf3bc65af3faa
Tornado <71680818+wanan-an@users.noreply.github.com>
2023-03-09 11:59:14 +0800

    Add files via upload

--- SWTC/SWTC.cpp
+++ SWTC/SWTC.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <map>
 #include <set>
+#include <random>
 #include <math.h>
 #include <inttypes.h>
 using namespace std;
@@ -38,8 +39,8 @@ map<int,int> m1;
 map<int,int> m2;
 int Triangle_num = 0;
 int Triangle_num_P = 0;
-int stream_num = 160000;
-int window_size = 4000000;
+int stream_num = 400000;
+int window_size = 8000000;
 set<Edge> Graph;
 map<int, set<int>> Adj;
 
@@ -61,7 +62,6 @@ uint32_t RSHash(const uint8_t* str, uint32_t len)
 
 uint32_t DEKHash(const uint8_t* str, uint32_t len)   
 {   
-    
     uint32_t hash = 1315423911;
 	for(int i = 0; i < len; i++){
 		hash = ((hash << 5) ^ (hash >> 27)) ^ str[i];
@@ -71,24 +71,42 @@ uint32_t DEKHash(const uint8_t* str, uint32_t len)
 }
 
 
+int count_distinct_edges(int starttime, int endtime){
+    set<pair<int,int>> distinct_edges;
+    for (int i = starttime+1; i <= endtime; i++){
+        distinct_edges.insert({edge_list[i].startnode,edge_list[i].endnode});
+    }
+    return distinct_edges.size();
+}
 
-void preoperation() {        //预处理，将边的信息存入列表中
-    int graphsize;
+void save_list(string filename){
+    ofstream outfile;
+    filename.erase(filename.end()-4,filename.end());
+    filename = filename + "1.txt";
+    outfile.open(filename,ios::out);
+    for (auto i : edge_list){
+        outfile << i.startnode << " " << i.endnode << endl;
+    }
+    outfile.close();
+}
+
+
+int preoperation() {        //预处理，将边的信息存入列表中
+    string filename;
     int number = 1;
-    cout << "input the number:";
-    cin >> graphsize;
-    graph.resize(graphsize);
+    cout << "input the filename:";
+    getline(cin,filename);
     old_list.resize(stream_num);
     new_list.resize(stream_num);
     sample_list.resize(stream_num);
     test_list.resize(stream_num);
     P_list.resize(stream_num);
-    string filename = "PA_" + to_string(graphsize) + "_edge.txt";
+    filename = filename + ".txt";
     string line;
     ifstream _csvinput(filename, ios::in);
     if (!_csvinput) {
         cout << "no such file!" << endl;
-        return;
+        return 0;
     }
     getline(_csvinput, line);
     int num = 0;
@@ -99,77 +117,70 @@ void preoperation() {        //预处理，将边的信息存入列表中
         number++;
         istringstream sin(line);
         string t;
-        int index = 0;
-        int startnode, endnode;
+        uint32_t index = 0;
+        uint32_t startnode, endnode;
         Edge e;
         while (sin >> t) {
             if (index == 0) {        //初始结点
-                e.startnode = stoi(t);
+                e.startnode = (uint32_t)stoul(t);
                 index++;
             }
             else {                   //目标结点，并将边存入列表
-                e.endnode = stoi(t);
+                e.endnode = (uint32_t)stoul (t);
                 if (e.startnode != e.endnode){
+                    if (e.startnode < e.endnode){
+                        uint32_t temp = e.startnode;
+                        e.startnode = e.endnode;
+                        e.endnode = temp;
+                    }
                     edge_list.push_back(e);
                 }
                 break;
             }
         }
     }
+    mt19937 gen(random_device{}());
+    shuffle(edge_list.begin(),edge_list.end(),gen);
+    save_list(filename);
+    return 1;
 }
 
-
+//当边e插入样本图时，增加对应的三角形数量
 void IncreaseTriangle(Edge e){
     int startnode = e.startnode;
     int endnode = e.endnode;
     set<int> combine;
     set_intersection(Adj[startnode].begin(),Adj[startnode].end(),Adj[endnode].begin(),Adj[endnode].end(),inserter(combine,combine.begin()));
-    Triangle_num += combine.size();
-    // for (auto i : combine){
-    //     cout << "公共邻居为:" << i << " ";
-    // }    
-    // cout << "三角形数量增加" << combine.size() << endl;
-
-    
+    Triangle_num += combine.size();    
 }
 
+//当边e退出样本图时，减少对应的三角形数量
 void DecreaseTriangle(Edge e){
     int startnode = e.startnode;
     int endnode = e.endnode;
     set<int> combine;
     set_intersection(Adj[startnode].begin(),Adj[startnode].end(),Adj[endnode].begin(),Adj[endnode].end(),inserter(combine,combine.begin()));
     Triangle_num -= combine.size();
-    // for (auto i : combine){
-    //     cout << "公共邻居为:" << i << " ";
-    // }
-    // cout << "三角形数量减少" << combine.size() << endl;
 }
 
-
+//将边e插入样本图
 void add_edge(Edge e){
-    // cout << "插入边<" << e.startnode << "," << e.endnode << "," << e.time << "," << e.value << "> "<< endl;
     Graph.insert(e);
-    if (Adj[e.startnode].find(e.endnode)==Adj[e.startnode].end()){
-        Adj[e.startnode].insert(e.endnode);
-        Adj[e.endnode].insert(e.startnode);
-        IncreaseTriangle(e);
-    }
-    // cout << "插入样本边为<" << e.startnode << "," << e.endnode << "," << e.time << "," << e.value <<  ">" << endl;
+    Adj[e.startnode].insert(e.endnode);
+    Adj[e.endnode].insert(e.startnode);
+    IncreaseTriangle(e);
 }
 
+//将边e从样本图删去
 void delete_edge(Edge e){
-    // cout << "删除边<" << e.startnode << "," << e.endnode << "," << e.time << "," << e.value << "> " << endl;
+    //边e为空或不在样本图中
     if (e.value == 0 || Graph.find(e) == Graph.end()){
-        // cout << "原样本为空，无需删除" << endl;
         return;
     }
     Graph.erase(e);
-    if (Adj[e.startnode].find(e.endnode)!=Adj[e.startnode].end()){
-        Adj[e.startnode].erase(e.endnode);
-        Adj[e.endnode].erase(e.startnode);
-        DecreaseTriangle(e);
-    }
-    // cout << "删除样本边为<" << e.startnode << "," << e.endnode << "," << e.time << "," << e.value <<  ">" << endl;
+    Adj[e.startnode].erase(e.endnode);
+    Adj[e.endnode].erase(e.startnode);
+    DecreaseTriangle(e);
 }
 
 
@@ -179,14 +190,14 @@ uint32_t allocate_position(uint32_t i1, uint32_t i2){
     string m = to_string(max(i1,i2));
     string s = to_string(min(i1,i2));
     string e = m+s;
-    return DEKHash((const uint8_t*) e.c_str(), e.length()) % stream_num;
+    return RSHash((const uint8_t*) e.c_str(), e.length()) % stream_num;
 }
 
 double allocate_value(uint32_t i1, uint32_t i2){
     string m = to_string(max(i1,i2));
     string s = to_string(min(i1,i2));
     string e = m+s;
-    return (double)(RSHash((const uint8_t*) e.c_str(), e.length()) % 1000000+1)/1000001;
+    return (double)(DEKHash((const uint8_t*) e.c_str(), e.length()) % 1000000+1)/1000001;
 }
 
 int compare_egde(Edge e1, Edge e2){
@@ -196,12 +207,6 @@ int compare_egde(Edge e1, Edge e2){
     return (e1.startnode == e2.startnode);
 }
 
-/*
-样本图数据结构的设计
-[0,1,2,3,4]
-0->[1,3,]
-[[1,3],[2,3]]
-SWTC方法的实现*/
 
 
 void BPS(vector<Edge> edge_list){
@@ -210,11 +215,11 @@ void BPS(vector<Edge> edge_list){
     while (current_time < edge_num){
         if (current_time % 100000 == 0){
             cout << " time=" << current_time;
-        }
-        int position = ((rand()<<15)+rand())%stream_num;    //扩大散列的范围
+        }        
         Edge current_edge = edge_list[current_time];
+        int position = allocate_position(current_edge.startnode, current_edge.endnode);
         current_edge.time = current_time;
-        current_edge.value = rand()%30000+1;
+        current_edge.value = allocate_value(current_edge.startnode, current_edge.endnode);        //使用标准的hash函数
         for (int i = 0; i < stream_num; i++){
             if (sample_list[i].value != 0 && sample_list[i].time <= current_time-window_size){
                 test_list[i] = sample_list[i];
@@ -240,25 +245,11 @@ void BPS(vector<Edge> edge_list){
                 }
             }
         }
-        //每秒计数一次
-        // for (int i = 0; i < stream_num; i++){
-        //     //有采样的情况：采样边优先级大于等于测试边且采样边不为空
-        //     if (sample_list[i].value > 0 && sample_list[i].value >= test_list[i].value){
-        //         sample_num_bps[current_time]++;
-        //     }
-        // }
         current_time++;
     }
 }
 
 
-void printGraph(){
-    for (int i = 0; i < stream_num; i++){
-        cout << "old边为<" << old_list[i].startnode << "," << old_list[i].endnode << "," << old_list[i].time << "," << old_list[i].value <<  ">" << endl;
-        cout << "new边为<" << new_list[i].startnode << "," << new_list[i].endnode << "," << new_list[i].time << "," << new_list[i].value <<  ">" << endl;
-    }
-    cout << endl;
-}
 
 double calculate(){
     double result = 0;
@@ -273,7 +264,7 @@ double calculate(){
 
 void SWTC(vector<Edge> edge_list){
     int edge_num = edge_list.size();
-    int current_time = 0;
+    int current_time = 1;
     double alpha = 0.7213/(1+1.079/stream_num);
     cout << "size:" << edge_num << endl;
     while (current_time < edge_num){
@@ -286,10 +277,8 @@ void SWTC(vector<Edge> edge_list){
         }
         Edge current_edge = edge_list[current_time];
         int position = allocate_position(current_edge.startnode, current_edge.endnode);
-        // int position =((rand()<<15)+rand())%stream_num;
         current_edge.time = current_time;
-        current_edge.value = allocate_value(current_edge.startnode, current_edge.endnode);        //使用标准的hash函数
-        // current_edge.value = (rand()+1)/(float)(RAND_MAX+1);        //使用标准的hash函数
+        current_edge.value = allocate_value(current_edge.startnode, current_edge.endnode);        
         if (compare_egde(current_edge, new_list[position])){
             set<Edge>::iterator it = Graph.find(new_list[position]);
             new_list[position].time = current_time;
@@ -300,58 +289,35 @@ void SWTC(vector<Edge> edge_list){
         }
         else{
             if (current_edge.value >= new_list[position].value){
-                // cout << "更新新切片" << "! ";
                 delete_edge(new_list[position]);
                 new_list[position] = current_edge;
                 if (new_list[position].value >= old_list[position].value){
-                    // if (new_list[position].value == old_list[position].value){
-                    //     cout << "new边为<" << new_list[position].startnode << "," << new_list[position].endnode << "," << new_list[position].time << "," << new_list[position].value <<  ">" << endl;
-                    //     cout << "old边为<" << old_list[position].startnode << "," << old_list[position].endnode << "," << old_list[position].time << "," << old_list[position].value <<  ">" << endl;
-                    // }
-                    // cout << "替换样本" << "! ";
                     delete_edge(old_list[position]);
                     add_edge(new_list[position]);
                 }
             }
         }
-        // if (current_edge.value >= new_list[position].value){
-        //         // cout << "更新新切片" << "! ";
-        //         delete_edge(new_list[position]);
-        //         new_list[position] = current_edge;
-        //         if (new_list[position].value >= old_list[position].value){
-        //             // cout << "替换样本" << "! ";
-        //             delete_edge(old_list[position]);
-        //             add_edge(new_list[position]);
-        //         }
-        // }
-        if (current_time % window_size == 0 && current_time > 0){   //完成新边的比较后，原来的新切片变成旧切片，并将新切片置空
+        //新旧切片替换
+        if (current_time % window_size == 0 && current_time > 0){   
             for (int i = 0; i < stream_num; i++){
                 if (new_list[i].value < old_list[i].value && new_list[i].value > 0){
-                    // cout << i << "号子流补充采样" << " ";
                     add_edge(new_list[i]);
                 }
                 old_list[i] = new_list[i];
                 new_list[i].value = 0;
                 new_list[i] = *new Edge();
             }
-            // cout << "子图大小为：" << Graph.size() << endl;
         }
-        // sample_num_swtc[current_time]=Graph.size();
-        // cout << "time: " << current_time << " Graph size: " << Graph.size() << " Triangle num: " << Triangle_num << endl;
-        // if (current_time % (window_size / 10) == 0) {
-        //     printGraph();
-        // }
         //每1/10个窗口计数一次
         if (current_time % (window_size / 10) == 0 && current_time > 0) {
-            cout << "time: " << current_time;
+            int starttime = ((current_time-window_size)<0)?0:(current_time-window_size);
+            int true_n = count_distinct_edges(starttime,current_time);
+            cout << " time: " << current_time;
             int m = Graph.size();
-            // cout << "m" << m << endl;
-            // cout << "b" << calculate() << endl;
             int n = (double(alpha*stream_num*m)/calculate());
-            cout << " n: " << n ;
+            cout << " n: " << n  << " true distinct num: " << true_n;
             sample_num_swtc[current_time]=Graph.size();
             double p = (double(m)/n)*(double(m-1)/(n-1))*(double(m-2)/(n-2));
-            // cout << p << endl;
             int count = Triangle_num/p;
             cout << " Graph size: " << Graph.size() << " Triangle num: " << Triangle_num << " True num: " << count << endl;
 
@@ -383,6 +349,8 @@ void save_result(){
         // cout << i.second << " ";
         outfile1 << i.second / float(stream_num) << " ";
     }
+    outfile1.close();
+    outfile2.close();
 }
 
 void save(){
@@ -405,12 +373,17 @@ void save(){
     for (auto i : m2){
         outfile2 << i.second << " " ;
     }
+    outfile1.close();
+    outfile2.close();
+
 }
 
 
 int main(){
-    srand(time(NULL));
-    preoperation();
+    srand(time(nullptr));
+    if (!preoperation()){
+        return 0;
+    }
     SWTC(edge_list);
     // BPS(edge_list); 
     save_result();
